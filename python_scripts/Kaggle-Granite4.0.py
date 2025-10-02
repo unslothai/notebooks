b9b4f8b304ab76a4bad32ae46932679cbc634f96
Erland366 <erland.pg366@gmail.com>
2025-10-02 13:02:00 +0000

    Fix spacing

--- python_scripts/Kaggle-Granite4.0.py
+++ python_scripts/Kaggle-Granite4.0.py
@@ -42,7 +42,6 @@
 # 
 # 
 # # ### Unsloth
-# 
 
 # In[ ]:
 
@@ -68,9 +67,9 @@ fourbit_models = [
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = "unsloth/granite-4.0-h-micro",
-    max_seq_length = 2048, # Choose any for long context!
-    load_in_4bit = False,  # 4 bit quantization to reduce memory
-    load_in_8bit = False, # [NEW!] A bit more accurate, uses 2x memory
+    max_seq_length = 2048,   # Choose any for long context!
+    load_in_4bit = False,    # 4 bit quantization to reduce memory
+    load_in_8bit = False,    # [NEW!] A bit more accurate, uses 2x memory
     full_finetuning = False, # [NEW!] We have full finetuning now!
 )
 
