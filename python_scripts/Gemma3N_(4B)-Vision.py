3f677304ed3e68d8da288bb605ec4d5a03af61fd
Aniket Maurya <theaniketmaurya@gmail.com>
2025-10-08 13:40:57 +0100

    revert

--- python_scripts/Gemma3N_(4B)-Vision.py
+++ python_scripts/Gemma3N_(4B)-Vision.py
@@ -96,6 +96,10 @@ model = FastVisionModel.get_peft_model(
     use_rslora = False,               # We support rank stabilized LoRA
     loftq_config = None,               # And LoftQ
     target_modules = "all-linear",    # Optional now! Can specify a list if needed
+    modules_to_save=[
+        "lm_head",
+        "embed_tokens",
+    ],
 )
 
 
