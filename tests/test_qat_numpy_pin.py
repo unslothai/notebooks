7f74dda4cad5f62e04af0feffdacb5453548dcbc
danielhanchen <unslothshared@gmail.com>
2026-08-04 19:55:44 +0000

    Add the AGPL-3.0 header to the new test file

--- tests/test_qat_numpy_pin.py
+++ tests/test_qat_numpy_pin.py
@@ -1,3 +1,16 @@
+# Unsloth Notebooks - Notebooks for Unsloth
+# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
 """The QAT numpy pin has to come from the generator, not the artifacts.
 
 fbgemm-gpu-genai depends on an unpinned numpy, so the force-reinstall in the
